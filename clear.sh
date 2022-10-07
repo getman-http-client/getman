a47d30a51bb886815fe6ff59bca09a6d17686d6a
Felipe Basso <felipe.basso@techer.com.br>
2022-10-06 21:26:20 -0300

    build and clear script

---
+++ clear.sh
@@ -0,0 +1,24 @@
+LICENSE_DIR=./.license
+if [ ! -d "$LICENSE_DIR" ]; then
+    mkdir .license
+fi
+
+README_FILE=./README.md
+if test -f "$README_FILE"; then
+    mv README.md ./.license
+fi
+LICENSE_FILE=./LICENSE
+if test -f "$LICENSE_FILE"; then
+    mv LICENSE ./.license
+fi
+
+rm -rf *.js
+rm -rf *.ico
+rm -rf *.txt
+rm -rf *.html
+rm -rf *.css
+rm -rf ./assets
+
+git add .
+git commit -m "Old build removed"
+git push
